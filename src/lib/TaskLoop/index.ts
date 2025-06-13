4d6c7f9456f381a7980b7e5833a46ad54ee6d6bd
rot1024 <aayhrot@gmail.com>
2025-06-13 16:29:23 +0900

    use ai sdk

--- src/lib/TaskLoop/index.ts
+++
@@ -1,9 +0,0 @@
-export { TaskLoop, createTaskLoop } from './TaskLoop';
-export { generateSystemPrompt } from './systemPrompt';
-export type {
-  Message,
-  TaskLoopConfig,
-  TaskLoopCallbacks,
-  TaskLoopState,
-  TaskLoopFunction,
-} from './types';
